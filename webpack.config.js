50d13bb9d0d7dd27ec6363c3006f79077822f0d0
congzhaoyang <congzhaoyang1@163.com>
2017-07-08 15:59:01 +0800

    first commit, add sass-loader, babel-loader, webpack-dev-server, plugins...

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle-[hash].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+        devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 9000
+    },
+    plugins: [
+        new HtmlWebpackPlugin(),
+        new OpenBrowserPlugin({ url: 'http://localhost:9000' }),
+        ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                //'file-loader',
+                'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+                'image-webpack-loader'
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                }
+            },
+
+        ]
+    },
+};
